c14865656a3fa38cfb4d08b7d670ffcd4001bf54
ortacu <72772410+ortacu@users.noreply.github.com>
2020-11-12 16:08:40 -0600

    Create grp-mbr.tf

---
+++ grp-mbr.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.user_one.name,
+    aws_iam_user.user_two.name,
+  ]
+
+  group = aws_iam_group.group.name
+}
